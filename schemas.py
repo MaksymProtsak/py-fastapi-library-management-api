e1a939bb30579452579f72ecc3acd3d4f10dc437
Maksym Protsak <maksym.protsak@gmail.com>
2025-01-09 13:54:03 +0200

    Created BookBase, BookCreate, BookDetail, AuthorBase, AuthorCreate, AuthorDetail classes in schemas.py.

---
+++ schemas.py
@@ -0,0 +1,39 @@
+from datetime import date
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str
+    publication_date: date
+
+
+class BookCreate(BookBase):
+    author_id: int
+
+
+class BookDetail(BookBase):
+    id: int
+    author: Optional["AuthorBase"]
+
+    class Config:
+        orm_mode = True
+
+
+class AuthorBase(BaseModel):
+    name: str
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class AuthorDetail(AuthorBase):
+    id: int
+    books: List[BookBase] = []
+
+    class Config:
+        orm_mode = True

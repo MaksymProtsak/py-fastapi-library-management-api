c2e48bd73fe6f8c778ef647ac73a59308f0a3da9
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2025-01-10 08:33:53 +0200

    Created get_author_by_name, create_author functions.

--- crud.py
+++ crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 import models
+import schemas
 
 
 def get_all_authors(db: Session):
@@ -8,3 +9,22 @@ def get_all_authors(db: Session):
 
 def get_author(db: Session, author_id: int):
     return db.query(models.DBAuthor).filter(models.DBAuthor.id == author_id).first()
+
+
+def get_author_by_name(db: Session, name: str):
+    return (
+        db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
+    )
+
+
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = models.DBAuthor(
+        name=author.name,
+        bio=author.bio,
+
+    )
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+
+    return db_author

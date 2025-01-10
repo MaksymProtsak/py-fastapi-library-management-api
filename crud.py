c69a5343718237dec474847d6a8dab6e6673b95b
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2025-01-10 08:14:29 +0200

    Created get_author.

--- crud.py
+++ crud.py
@@ -4,3 +4,7 @@ import models
 
 def get_all_authors(db: Session):
     return db.query(models.DBAuthor).all()
+
+
+def get_author(db: Session, author_id: int):
+    return db.query(models.DBAuthor).filter(models.DBAuthor.id == author_id).first()

2c3ea61178473392e151164ec0b2bf651ed4b08b
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2025-01-10 08:20:51 +0200

    Crated read_single_author.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 
 from sqlalchemy.orm import Session
 
@@ -26,3 +26,13 @@ def root():
 @app.get("/authors/", response_model=list[schemas.Author])
 def get_all_authors(db: Session = Depends(get_db)):
     return crud.get_all_authors(db=db)
+
+
+@app.get("/authors/{author_id}/", response_model=schemas.Author)
+def read_single_author(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.get_author(db=db, author_id=author_id)
+
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+
+    return db_author

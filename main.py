3a19284a4622dcdae8954198a3e0db75454bbf3b
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2025-01-10 07:52:30 +0200

    Crated get_db, get_all_authors.

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from sqlalchemy.orm import Session
+
+import crud
+import schemas
+from database import SessionLocal
 
 app = FastAPI()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
 def root():
     return {"message": "Hello"}
+
+
+@app.get("/authors/", response_model=list[schemas.Author])
+def get_all_authors(db: Session = Depends(get_db)):
+    return crud.get_all_authors(db=db)

14903ec8695f61a0dfe9989c591fecdf857d856b
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2025-01-10 07:54:28 +0200

    Updated relationships link, used str type unlike link to model class.

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class DBAuthor(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), nullable=False, unique=True)
     bio = Column(String(511), nullable=False)
-    books = relationship("Book", back_populates="author")
+    books = relationship("DBBook", back_populates="author")
 
 
 class DBBook(Base):
@@ -27,4 +27,4 @@ class DBBook(Base):
     publication_date = Column(Date, nullable=False)
     author_id = Column(Integer, ForeignKey("authors.id"))
 
-    author = relationship(DBAuthor, back_populates="books")
+    author = relationship("DBAuthor", back_populates="books")
